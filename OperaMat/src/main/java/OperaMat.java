7288dd65f27657e038859a7517b1b28aa6507796
NiloRlz <1459566@senati.pe>
2023-08-18 18:51:04 -0500

    agregar scanner

--- OperaMat/src/main/java/OperaMat.java
+++ OperaMat/src/main/java/OperaMat.java
@@ -2,15 +2,10 @@
 IMPORTAR LA LIBRERIA SCANNER
 */
 import java.util.Scanner;
-package com.mycompany.operamat;
 
-/**
- *
- * @author Alumno
- */
 public class OperaMat {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+ 
     }
 }
