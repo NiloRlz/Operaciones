22fd007d3a8d310b93385de5f5643753ec157cc8
NiloRlz <1459566@senati.pe>
2023-08-18 18:59:47 -0500

    agregar datos de entrada

--- OperaMat/src/main/java/OperaMat.java
+++ OperaMat/src/main/java/OperaMat.java
@@ -6,6 +6,14 @@ import java.util.Scanner;
 public class OperaMat {
 
     public static void main(String[] args) {
+        /*
+        AGREGAR TIPOS DE DATOS
+        */
+    Scanner input = new Scanner(System.in);
+    System.out.print("Ingrese el primer número: ");
+    double num1 = input.nextDouble();
+    System.out.print("Ingrese el segundo número: ");
+    double num2 = input.nextDouble();
  
     }
 }
